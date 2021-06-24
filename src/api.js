ff83cd2bcfa21117d858bdf193068aed19708a1a
Mahdiyeh <mahdiyeh.fani@gmail.com>
2021-06-24 21:44:17 +0430

    add env file and change the api file

--- src/api.js
+++ src/api.js
@@ -1,6 +1,5 @@
 //Base URL
 const base_url = "https://api.rawg.io/api/";
-const APP_KEY = "3c1964cf519c4db9b5df3b6ea3e59d46";
 
 //Getting the date
 const getCurrentMonth = () => {
@@ -30,9 +29,9 @@ const lastYear = `${currentYear - 1}-${currentMonth}-${currentDay}`;
 const nextYear = `${currentYear + 1}-${currentMonth}-${currentDay}`;
 
 //Popular Games
-const popular_games = `games?key=${APP_KEY}&dates=${lastYear},${currentDate}&ordering=-rating&page_size=10`;
-const upcoming_games = `games?key=${APP_KEY}&dates=${currentDate},${nextYear}&ordering=-added&page_size=10`;
-const newGames = `games?key=${APP_KEY}&dates=${lastYear},${currentDate}&ordering=-released&page_size=10`;
+const popular_games = `games?key=${process.env.REACT_APP_API_KEY}&dates=${lastYear},${currentDate}&ordering=-rating&page_size=10`;
+const upcoming_games = `games?key=${process.env.REACT_APP_API_KEY}&dates=${currentDate},${nextYear}&ordering=-added&page_size=10`;
+const newGames = `games?key=${process.env.REACT_APP_API_KEY}&dates=${lastYear},${currentDate}&ordering=-released&page_size=10`;
 
 export const popularGamesURL = () => `${base_url}${popular_games}`;
 export const upcomingGamesURL = () => `${base_url}${upcoming_games}`;
@@ -40,10 +39,10 @@ export const newGamesURL = () => `${base_url}${newGames}`;
 //GAME DETAILS
 
 export const gameDetailsURL = (game_id) =>
-  `${base_url}games/${game_id}?key=${APP_KEY}`;
+  `${base_url}games/${game_id}?key=${process.env.REACT_APP_API_KEY}`;
 
 export const gameScreenshoutURL = (game_id) =>
-  `${base_url}games/${game_id}/screenshots?key=${APP_KEY}`;
+  `${base_url}games/${game_id}/screenshots?key=${process.env.REACT_APP_API_KEY}`;
 
 export const searchedGames = (game_name) =>
-  `${base_url}games?search=${game_name}&page_size=10&key=${APP_KEY}`;
+  `${base_url}games?search=${game_name}&page_size=10&key=${process.env.REACT_APP_API_KEY}`;
